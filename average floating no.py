7392e7ca1c12034ad726c5b106e721b7b7a3a905
Aman Rawat <amanayush1hg1@gmail.com>
2025-08-31 19:33:29 +0530

    Update average floating no.py

--- average floating no.py
+++ average floating no.py
@@ -1,8 +1,8 @@
 
-
-# input sloats
+# input sloats----------
 
 a = float(input("Add 1st No. : "))
 b = float(input("Add 2nd No. : "))
 
+# output ---------
 print("Average of both No." , (float(a)+float(b))/2)
